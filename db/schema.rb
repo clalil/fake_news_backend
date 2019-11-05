c7269e0e835224457d4e30422aa2046651a55d10
clalil <cliljander@gmail.com>
2019-11-05 18:12:52 +0100

    Migrate database after upstream pull.

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2019_11_01_180656) do
     t.integer "journalist_id"
   end
 
+  create_table "journalists", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_journalists_on_article_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -76,4 +83,5 @@ ActiveRecord::Schema.define(version: 2019_11_01_180656) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "journalists", "articles"
 end
